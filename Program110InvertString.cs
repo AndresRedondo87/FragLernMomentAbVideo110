88e4703628faec491492178c8c301220f9675225
AndresRedondo87 <66962177+AndresRedondo87@users.noreply.github.com>
2020-09-15 13:26:03 +0200

    Video 111 - Der Stack (Keller) und Methode - Theorie Erklärung und Beispiel dafuer kommentiert.

--- Program110InvertString.cs
+++ Program110InvertString.cs
@@ -1,46 +1,46 @@
-﻿using System;
+﻿//using System;
 
-namespace FragLernMoment_1_InvertString
-{
-    // Frage von Zaidi: Bitte erkläre mir die Logik der Methode!
-    // Selber angepasst - Bedienung, Logik bleibt genau gleich
-    class Program
-    {
+//namespace FragLernMoment_1_InvertString
+//{
+//    // Frage von Zaidi: Bitte erkläre mir die Logik der Methode!
+//    // Selber angepasst - Bedienung, Logik bleibt genau gleich
+//    class Program
+//    {
 
-        static void Main(string[] args)     // angepasste Bedienung
-        {
-            string text;
-            do
-            {
+//        static void Main(string[] args)     // angepasste Bedienung
+//        {
+//            string text;
+//            do
+//            {
 
-                Console.WriteLine("Schreiben Sie einen Text um es zu invertieren[ENTER zu beenden]: ");
-                text = Console.ReadLine();
-                string invertText = Invert(text);
-                Console.WriteLine($"\nDer Text war :\t\t" + text + " \nInvertiert ist:\t\t" + invertText + Environment.NewLine + Environment.NewLine);
-            } while (!String.IsNullOrEmpty(text));
-        }
+//                Console.WriteLine("Schreiben Sie einen Text um es zu invertieren[ENTER zu beenden]: ");
+//                text = Console.ReadLine();
+//                string invertText = Invert(text);
+//                Console.WriteLine($"\nDer Text war :\t\t" + text + " \nInvertiert ist:\t\t" + invertText + Environment.NewLine + Environment.NewLine);
+//            } while (!String.IsNullOrEmpty(text));
+//        }
 
 
-        //Methode to invert text
-        static string Invert(string text)
-        {
-            /// Zaidi´s Loesung
-            //erzeugt temporäres Feld von chars - wahrscheinlich um das resultat zu speichern
-            char[] myChar = new char[text.Length];
+//        //Methode to invert text
+//        static string Invert(string text)
+//        {
+//            /// Zaidi´s Loesung
+//            //erzeugt temporäres Feld von chars - wahrscheinlich um das resultat zu speichern
+//            char[] myChar = new char[text.Length];
 
-            for (int i = 1; i <= text.Length; i++)
-            {
-                myChar[text.Length - i] = text[i - 1];
-                Console.WriteLine($"(Interne Schritte {i}: {new string(myChar)})");
-            }
+//            for (int i = 1; i <= text.Length; i++)
+//            {
+//                myChar[text.Length - i] = text[i - 1];
+//                Console.WriteLine($"(Interne Schritte {i}: {new string(myChar)})");
+//            }
 
-            return new string(myChar);
+//            return new string(myChar);
 
-            // Lehrer´s Loesung - er kennt Viele Klassen und Methoden, das macht alles einfacher.
-            //char[] myChar = text.ToCharArray();
-            //Array.Reverse(myChar);
+//            // Lehrer´s Loesung - er kennt Viele Klassen und Methoden, das macht alles einfacher.
+//            //char[] myChar = text.ToCharArray();
+//            //Array.Reverse(myChar);
 
-            //return new string(myChar);
-        }
-    }
-}
+//            //return new string(myChar);
+//        }
+//    }
+//}
--- Program111DerStackUndMethodenAufruf.cs
+++ Program111DerStackUndMethodenAufruf.cs
@@ -1,44 +1,171 @@
-﻿//using System;
+﻿using System;
 
-//namespace FragLernMoment_2_AddiereZahlen
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            string ersteZahlAlsString;
-//            string zweiteZahlAlsString;
-//            double ersteZahl;
-//            double zweiteZahl;
-//            double resultat;
+namespace FragLernMoment_2_AddiereZahlen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ersteZahlAlsString;
+            string zweiteZahlAlsString;
+            double ersteZahl;
+            double zweiteZahl;
+            double resultat;
 
-//            // Zahlen vom Benutzer eingeben lassen
-//            Console.Write("Bitte gib die erste Zahl ein: ");
-//            ersteZahlAlsString = Console.ReadLine();
-//            Console.Write("Bitte gib die zweite Zahl ein: ");
-//            zweiteZahlAlsString = Console.ReadLine();
+            // Zahlen vom Benutzer eingeben lassen
+            Console.Write("Bitte gib die erste Zahl ein: ");
+            ersteZahlAlsString = Console.ReadLine();
+            Console.Write("Bitte gib die zweite Zahl ein: ");
+            zweiteZahlAlsString = Console.ReadLine();
 
-//            // Zahlen konvertieren
-//            ersteZahl = Convert.ToDouble(ersteZahlAlsString);
-//            zweiteZahl = Convert.ToDouble(zweiteZahlAlsString);
+            // Zahlen konvertieren
+            ersteZahl = Convert.ToDouble(ersteZahlAlsString);
+            zweiteZahl = Convert.ToDouble(zweiteZahlAlsString);
 
-//            // Methode aufrufen und nach der Ausführung das Resultat übernehmen
-//            resultat = Addiere(ersteZahl, zweiteZahl);
+            // Methode aufrufen und nach der Ausführung das Resultat übernehmen
+            resultat = Addiere(ersteZahl, zweiteZahl);
 
-//            // Ausgabe
-//            Console.WriteLine("Die Summe der beiden eingegebenen Zahlen ist: " + resultat);
+            // Ausgabe
+            Console.WriteLine("Die Summe der beiden eingegebenen Zahlen ist: " + resultat);
 
-//            // Benutzer Enter drücken lassen bevor Programm beendet wird
-//            Console.ReadLine();
-//        }
+            // Benutzer Enter drücken lassen bevor Programm beendet wird
+            Console.ReadLine();
+        }
 
-//        static double Addiere(double ersterSummand, double zweiterSummand)
-//        {
-//            // Eigentliche Berechnung
-//            double summe = ersterSummand + zweiterSummand;
+        static double Addiere(double ersterSummand, double zweiterSummand) //Verweise und Aufrufhierarchie koennen dafuer sehr Hilfreich sein
+        {
+            // Eigentliche Berechnung
+            double summe = ersterSummand + zweiterSummand;
 
-//            // Rückgabewert
-//            return summe;
-//        }
-//    }
-//}
+            // Rückgabewert
+            return summe;
+        }
+
+        /// mit LEGO Steie geklaert. THEORIE STACK
+        /// ganz am Anfang haben wir ganz unten ein Stein, der Stack ist.
+        /// Beim Konsolenanwendung Starten wird der StackFrame dadrauf angelegt.
+        /// es ist wie ein Kontainer wo Informationen drinne stehen. Der Stein symbolisiert diese Kontainer
+        ///  Alles was in die Methoden passiert, passiert in diese Kontainer drin, bzw auf den StackFrame drauf.
+        ///  das heisst in Methode Main werden speicherplaetze für Variablen gespeichert (im Beispiel schauen wir die Zahlen und resultat).
+        ///  da werden die werte zuweisugen auch erfolgen, z.B. die Werte eingabe 2 und 5 fuer die Benutzer Eingaben:
+        ///  ---
+        ///  [leer Resultat]
+        ///  [5]
+        ///  [2]
+        ///  [StackFrame]
+        ///  [HauptStein]
+        ///  
+        ///  Dann kommt die addiere Methode: 
+        ///  [AddiereMethode]
+        ///  [leer Resultat]
+        ///  [5]
+        ///  [2]
+        ///  [StackFrame]
+        ///  [HauptStein]
+        ///  
+        ///  Dann merkt es sich wo wir waren als wir die MEthode aufgerufen haben(Linie 26 in Beispiel) 
+        ///  [Merken, wenn Addieren Methode fertig ist, weiter ab Linie 26 weitermachen]
+        ///  [AddiereMethode]
+        ///  [leer Resultat]
+        ///  [5]
+        ///  [2]
+        ///  [StackFrame]
+        ///  [HauptStein]
+        ///  
+        ///  Dann die Methode nimmt die Parameter auf den Stack drauf, erstmal leer und reserviert Speicherplatz für die beide
+        ///  // und scheinbar auch fuer Ihre return wert.
+        ///  [leer Resultat]
+        ///  [leer Parameter ersterSummand]
+        ///  [leer Parameter zweiterSummand]
+        ///  [Merken, wenn Addieren Methode fertig ist, weiter ab Linie 26 weitermachen]
+        ///  [AddiereMethode]
+        ///  [leer Resultat]
+        ///  [5]
+        ///  [2]
+        ///  [StackFrame]
+        ///  [HauptStein]
+        ///  
+        ///  Dann die Methode kopiert die werte fuer die Parameter auf den neuen Speicherbereich(Stack-Steine)
+        ///  Die aendern nichts von der originale 5 und 2,seine Werte werden nur einfach drueberkopiert aus Main Methode
+        ///  [leer Resultat]
+        ///  [5]
+        ///  [2]
+        ///  [Merken, wenn Addieren Methode fertig ist, weiter ab Linie 26 weitermachen]
+        ///  [AddiereMethode]
+        ///  [leer Resultat]
+        ///  [5]
+        ///  [2]
+        ///  [StackFrame]
+        ///  [HauptStein]
+        ///  
+        ///  Dann geht es richtig in die Methode rein. Da ist die summe deklariert, so es bekommt ein euen Speicherbereich(Stack-Steine)
+        ///  [leer - summe]
+        ///  [5]
+        ///  [2]
+        ///  [Merken, wenn Addieren Methode fertig ist, weiter ab Linie 26 weitermachen]
+        ///  [AddiereMethode]
+        ///  [leer Resultat]
+        ///  [5]
+        ///  [2]
+        ///  [StackFrame]
+        ///  [HauptStein]
+        ///  
+        ///  Die Summe wird berechnet und auf summe gespeichert: 2+5 = 7
+        ///  [7]
+        ///  [5]
+        ///  [2]
+        ///  [Merken, wenn Addieren Methode fertig ist, weiter ab Linie 26 weitermachen]
+        ///  [AddiereMethode]
+        ///  [leer Resultat]
+        ///  [5]
+        ///  [2]
+        ///  [StackFrame]
+        ///  [HauptStein]
+        ///  
+        ///  Die Summe wird berechnet und auf summe gespeichert: 2+5 = 7
+        ///  eigentlich nur mit den Daten auf den letzten StackFrame(so zu sagen von addiereMethode Stein,nach oben.
+        ///  Nicht mit den daten aus Main die da drunter stehen, sie sind hierher ganz unabhaengig 
+        ///  Die Summe ist fertig so diese StackFrame muss abgebaut werden.
+        ///  Sie weisst zwei sachen:
+        ///  1- sie muss zurueck in Linie 26 weitermachen
+        ///  2- return, bzw den summe Wert zurueckgeben 
+        ///  mit diese Info wird diese StackFrame komplett abgebaut
+        ///  
+        ///  [leer Resultat]
+        ///  [5]
+        ///  [2]
+        ///  [StackFrame]
+        ///  [HauptStein]
+        ///  
+        ///  Dann sehen wir wieder in die Linie 26 dass die lokale Variable resultat bekommt der Wert aus der return von unsere methode,
+        ///  so wird es da zugewiesen.
+        ///  
+        ///  [7]
+        ///  [5]
+        ///  [2]
+        ///  [StackFrame]
+        ///  [HauptStein]
+        ///  So haben wir schon den Resultat Wert in unsere Stack, aber das ganze StackFrame von Addiere Methode ist abgebaut.  
+        ///  Wenn im Beispiel nochmal addieren aufgerufen würde, wurde es einfach nochmal genauso draufgesteckt (
+        ///  [leer - summe]
+        ///  [leer Parameter ersterSummand]
+        ///  [leer Parameter zweiterSummand]
+        ///  [Merken, wenn Addieren Methode fertig ist, weiter ab Linie 26 weitermachen]
+        ///  [AddiereMethode]   //NOCHMAL
+        ///  [7]
+        ///  [5]
+        ///  [2]
+        ///  [StackFrame]
+        ///  [HauptStein]
+        ///  Das ist in unser Beispiel aber nicht, so...
+        ///  [7]
+        ///  [5]
+        ///  [2]
+        ///  [StackFrame]
+        ///  [HauptStein]
+        ///  Damit wurde dan den resultat in Main einfach ausgegeben.
+        ///  Am Ende von Main wurde einfach alles abgebaut und Anwendung verschlossen.
+
+
+    }
+}
